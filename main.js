e1dd52d2e6784959da7f8899638fc9641cc938b1
rudziktv <auto75208@gmail.com>
2024-08-18 12:47:55 +0200

    project init

---
+++ main.js
@@ -0,0 +1,9 @@
+// import { initMap } from "https://unpkg.com/gna-map@1.0.1/index.js";
+
+// initMap("sds");
+
+document.addEventListener("DOMContentLoaded", () => {
+    // initMap("joo");
+    console.log("joo");
+    GNAMap.initMap("csl");
+});
